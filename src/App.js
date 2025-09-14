6aac032cd4d5b16ccfa1d51d767b105903eb89b9
Aphomer <aaphomer@gmail.com>
2025-09-14 20:51:14 +0100

    cut initiaItems from css to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
+
+
 export default function App() {
   return (
     <div className="app">
