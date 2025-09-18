b5163591212ee55d524cc51689b9e0fd8da31d41
Aphomer <aaphomer@gmail.com>
2025-09-18 22:22:16 +0100

    included app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 3, description: "shoe", quantity: 1, packed: false },
+  { id: 3, description: "Shoe", quantity: 1, packed: false },
 ];
 
 
