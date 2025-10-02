24e330cafdef02ef5a88b856cefa87db645f5ff6
Aphomer <aaphomer@gmail.com>
2025-10-02 20:36:28 +0100

    I HAVE TRIED MY BEST

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function Form() {
       <select>
         <option value={1}>1</option>
         <option value={2}>2</option>
-        <option value={3}>3</option>
+        {/* <option value={3}>3</option> */}
         {/* <option value={1}>1</option> */}
         {/* <option value={2}>2</option> */}
         {/* <option value={3}>3</option> */}
